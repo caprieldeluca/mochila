e8342a82a5921a02910e5e289cbcb66e677584f2
Gabriel De Luca <caprieldeluca@gmail.com>
2024-08-19 16:55:03 -0300

    Include function to return raster file writter in rlayers.

--- raster/rlayers.py
+++ raster/rlayers.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from qgis.core import QgsRasterLayer
+from qgis.core import (
+    QgsRasterFileWriter,
+    QgsRasterLayer,
+)
 from mochila import plog
 
 
@@ -14,4 +17,15 @@ def get_layer_from_geotiff(uri, baseName):
     layer = QgsRasterLayer(uri, baseName)
 
     return layer
+
+
+#####
+# Get Raster File Writer by extension
+#####
+def get_raster_file_writer(extension):
+    """Get the raster file writer object from extension.
+
+    Example: plog(get_raster_file_writer('tif')).
+    """
+    return QgsRasterFileWriter.driverForExtension(extension)
 
